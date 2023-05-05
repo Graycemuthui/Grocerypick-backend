2c7987f1c8c896ee6cc2ca661b085e4c2ab2cb54
Grayce Muthui <graycemuthui20@gmail.com>
2023-05-05 23:03:37 +0300

    Create the controllers

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,5 @@
 class OrdersController < ApplicationController
-  before_action :set_order, only: %i[ show edit update destroy ]
+  before_action :set_order, only: %i[show edit update destroy]
 
   # GET /orders or /orders.json
   def index
@@ -8,6 +8,8 @@ class OrdersController < ApplicationController
 
   # GET /orders/1 or /orders/1.json
   def show
+    @order = Order.find(params[:id])
+    render json: @order
   end
 
   # GET /orders/new
@@ -15,56 +17,40 @@ class OrdersController < ApplicationController
     @order = Order.new
   end
 
-  # GET /orders/1/edit
-  def edit
-  end
-
   # POST /orders or /orders.json
   def create
     @order = Order.new(order_params)
+    if @order.save
 
-    respond_to do |format|
-      if @order.save
-        format.html { redirect_to order_url(@order), notice: "Order was successfully created." }
-        format.json { render :show, status: :created, location: @order }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @order.errors, status: :unprocessable_entity }
-      end
+      render json: @order, status: :created, location: @order
+    else
+
+      render json: @order.errors, status: :unprocessable_entity
     end
   end
 
   # PATCH/PUT /orders/1 or /orders/1.json
   def update
-    respond_to do |format|
-      if @order.update(order_params)
-        format.html { redirect_to order_url(@order), notice: "Order was successfully updated." }
-        format.json { render :show, status: :ok, location: @order }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @order.errors, status: :unprocessable_entity }
-      end
+    @order = Order.find(params[:id])
+    if @order.update(order_params)
+
+      render json: @order
+    else
+
+      render json: @order.errors, status: :unprocessable_entity
     end
   end
 
   # DELETE /orders/1 or /orders/1.json
   def destroy
+    @order = Order.find(params[:id])
     @order.destroy
-
-    respond_to do |format|
-      format.html { redirect_to orders_url, notice: "Order was successfully destroyed." }
-      format.json { head :no_content }
-    end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_order
-      @order = Order.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def order_params
-      params.fetch(:order, {})
-    end
+  # Only allow a list of trusted parameters through.
+  def order_params
+    params.require(:order).permit(:customer_id, :order_total, :order_quantity, :order_date)
+  end
 end
