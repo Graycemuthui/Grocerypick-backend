a0c029b0b3fc306112eadd3bb657eff37ca0e2b3
Grayce Muthui <graycemuthui20@gmail.com>
2023-05-11 23:45:27 +0300

    Confirm routes

--- config/routes.rb
+++ config/routes.rb
@@ -11,8 +11,4 @@ Rails.application.routes.draw do
       resources :products
     end
   end
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
