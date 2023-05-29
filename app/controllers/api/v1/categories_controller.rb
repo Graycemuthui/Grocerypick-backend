28594d830f81bebac675c6a0115d04f6deb4f3c6
Grayce Muthui <graycemuthui20@gmail.com>
2023-05-29 22:17:36 +0300

    Fixed the categories controller

--- app/controllers/api/v1/categories_controller.rb
+++ app/controllers/api/v1/categories_controller.rb
@@ -1,14 +1,28 @@
 class Api::V1::CategoriesController < ApplicationController
   skip_before_action :authorized
-  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+  # rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
   def index
     @categories = Category.all
+    # for the category we want to list all the food items it contains and add that to the json response
+    @categories = @categories.as_json.map do |category|
+      # find all food items with the same category id as the current category
+      product = Product.where(category_id: category['id'])
+      # add the food items to the current category
+      category.merge({ products: product })
+    end
+
     render json: @categories
   end
 
   def show
     @category = Category.find(params[:id])
+    # for the category we want to list all the food items it contains and add that to the json response
+    @category = @category.as_json
+    # find all food items with the same category id as the current category
+    product = Product.where(category_id: @category['id'])
+    # add the food items to the current category
+    @category.merge!({ products: product })
     render json: @category
   end
 
